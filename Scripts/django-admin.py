bf6a61a39e97138e458b9f6b4e7136ab3defd33f
zekokoc <94122342+zekokoc@users.noreply.github.com>
2024-01-13 00:34:17 +0300

    Add files via upload

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\kale_\Projeler\django-ecommerce-master\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
